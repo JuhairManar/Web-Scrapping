8e98a840f2c217f465aac569e6608ac5d3281612
MD JUHAIAR MANAR CHOWDHURY <89413439+JuhairManar@users.noreply.github.com>
2024-01-12 22:58:09 +0600

    Add files via upload

--- Price and name list of bitcoins from a page/1.py
+++ Price and name list of bitcoins from a page/1.py
@@ -1,1 +1,22 @@
+from bs4 import BeautifulSoup
+import requests
 
+url="https://coinmarketcap.com"
+result=requests.get(url).text
+doc=BeautifulSoup(result,'html.parser') 
+
+
+tbody=doc.tbody
+trs=tbody.contents #table rows #all info as list
+prices={}
+
+for tr in trs:
+    if len(tr.contents)>=4:
+        name,price=tr.contents[2],tr.contents[3]
+        if name.p and price.a:
+            a=name.p.string  #p tag and its string
+            b=price.a.string
+            if a and b:
+                prices.update({a:b})
+
+#print(prices)    
